0e902d2697571b1239608d261a0f296ca929aa59
vrosalesjaimes <vrosalesjaimes@ciencias.unam.mx>
2023-07-14 14:33:52 -0600

    Starter project in springboot

---
+++ Backend/src/main/java/com/vrj/airportweather/AirportWeatherInformationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.vrj.airportweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportWeatherInformationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportWeatherInformationSystemApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/vrj/airportweather/AirportWeatherInformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrj.airportweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportWeatherInformationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
