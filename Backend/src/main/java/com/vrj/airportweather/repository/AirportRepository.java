a12a15e23297940a8e3b32782f27f47e3def6749
vrosalesjaimes <vrosalesjaimes@ciencias.unam.mx>
2023-07-16 12:34:53 -0600

    Remove @Repository annotation is not necessary

--- Backend/src/main/java/com/vrj/airportweather/repository/AirportRepository.java
+++ Backend/src/main/java/com/vrj/airportweather/repository/AirportRepository.java
@@ -2,11 +2,9 @@ package com.vrj.airportweather.repository;
 
 import com.vrj.airportweather.model.Airport;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Set;
 
-@Repository
 public interface AirportRepository extends JpaRepository<Airport, Long> {
     Set<Airport> findByCity(String city);
     Set<Airport> findByCountry(String country);
