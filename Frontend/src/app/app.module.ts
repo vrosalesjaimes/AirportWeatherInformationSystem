38f60fa7d57075df1bc44d141918ddb0e3e4f70e
vrosalesjaimes <vrosalesjaimes@ciencias.unam.mx>
2023-07-16 20:52:34 -0600

    Add header

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -7,17 +7,21 @@ import { FormComponent } from './component/form/form.component';
 
 import {MatSelectModule} from '@angular/material/select';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { HeaderComponent } from './component/header/header.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatSelectModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
