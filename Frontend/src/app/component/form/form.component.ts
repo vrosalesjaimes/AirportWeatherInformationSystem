3649c8c8c803ca5c9f9b07dfb6f916e6331c4017
vrosalesjaimes <vrosalesjaimes@ciencias.unam.mx>
2023-07-21 20:44:21 -0600

    Modification so that only one form is created, panel refactoring.

--- Frontend/src/app/component/form/form.component.ts
+++ Frontend/src/app/component/form/form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-form',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent {
-
+  @Input() title!: string;
+  
 }
