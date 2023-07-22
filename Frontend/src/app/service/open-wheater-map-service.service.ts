ffa395564b30595d9e208e556e9e1bd92b1aba6f
vrosalesjaimes <vrosalesjaimes@ciencias.unam.mx>
2023-07-22 14:26:41 -0600

    OpenWheaterMap service create

---
+++ Frontend/src/app/service/open-wheater-map-service.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OpenWheaterMapServiceService {
+  private apiKey: string = environment.openWeatherMapApiKey;
+  private baseUrl: string = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getWeatherByCoordinates(latitude: string, longitude: string): Observable<any> {
+    const params = new HttpParams()
+      .set('lat', latitude)
+      .set('lon', longitude)
+      .set('exclude', 'minutely,hourly,daily')
+      .set('appid', this.apiKey);
+
+    return this.http.get<any>(this.baseUrl, { params });
+  }
+}
