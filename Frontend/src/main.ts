49d6c28de3e81b701f0b5514a0ba872015b3f030
vrosalesjaimes <vrosalesjaimes@ciencias.unam.mx>
2023-07-23 17:43:20 -0600

    add loader

--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,7 +1,17 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { ElementRef, ViewChild } from '@angular/core';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+      const appRootElement = document.querySelector('app-root');
+      if (appRootElement) {
+        appRootElement.classList.remove('content'); 
+      }
+    }, 1500);
+  });
